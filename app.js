21f9772cdc616164b78b25efde94f8c241fedfe8
Eric Abell <ericabell@users.noreply.github.com>
2017-08-16 14:16:02 -0500

    Passed simple todo list into tempate

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const expressValidator = require('express-validator');
 
+let todos = {
+  todos : [
+    {name: 'todo 1', completed: true},
+    {name: 'todo 2', completed: false},
+    {name: 'todo 3', completed: false},
+  ]
+}
+
 let app = express();
 
 app.engine('mustache', mustacheExpress());
@@ -13,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(expressValidator());
 
 app.get('/', (req,res) => {
-  res.render('todos', {test: 'this is a test'});
+  res.render('todos', todos);
 })
 
 app.listen(3000, () => {
