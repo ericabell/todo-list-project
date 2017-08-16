292e397d2fbf437e63293aaa12706b7386c6b027
Eric Abell <ericabell@users.noreply.github.com>
2017-08-16 14:56:39 -0500

    separated list of completed and not completed

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const expressValidator = require('express-validator');
 
 let todos = {
   todos : [
-    {id: 1, name: 'todo 1', completed: true},
+    {id: 1, name: 'todo 1', completed: false},
     {id: 2, name: 'todo 2', completed: false},
-    {id: 3, name: 'todo 3', completed: false},
+    {id: 3, name: 'todo 3', completed: true}
   ]
 }
 
 let app = express();
+
+app.use(express.static('public'));
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
